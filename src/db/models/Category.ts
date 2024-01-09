0beaf83db2b30de4fc531641c1c5f52d57d8a5a1
Fabricio Nascimento <fabriciocunha040@gmail.com>
2024-01-09 17:20:25 -0300

    :label: feat: Create model for categories table

---
+++ src/db/models/Category.ts
@@ -0,0 +1,25 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+
+import sequelize from '../config.js';
+
+export interface CategoryAttributes {
+  id: number;
+  name: string;
+}
+
+export interface CategoryCreationAttributes extends Optional<CategoryAttributes, 'id'> {}
+
+export interface CategoryInstance extends Model<CategoryAttributes, CategoryCreationAttributes>, CategoryAttributes {}
+
+export const Category = sequelize.define<CategoryInstance, CategoryAttributes>('categories', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+});
