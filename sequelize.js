fa7c468993e66ec4bd2d168309313e2764db656f
Fabricio Nascimento <fabriciocunha040@gmail.com>
2024-01-09 17:39:27 -0300

    :wrench: chore: Refactor database configuration and Sequelize setup

--- sequelize.js
+++ sequelize.js
@@ -1,9 +1,16 @@
-module.exports = {
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+const config = {
   development: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT,
   },
 };
+
+export default config;
--- src/db/config.ts
+++ src/db/config.ts
@@ -1,5 +1,15 @@
-import { Sequelize } from 'sequelize';
+import { Dialect, Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: process.env.DATABASE_DIALECT as any });
+dotenv.config({ path: '.env.local' });
 
-export default sequelize;
+const database = new Sequelize({
+  username: process.env.DATABASE_USER as string,
+  password: process.env.DATABASE_PASSWORD as string,
+  dialect: process.env.DATABASE_DIALECT as Dialect,
+  host: process.env.DATABASE_HOST as string,
+  database: process.env.DATABASE_NAME as string,
+  port: parseInt(process.env.DATABASE_PORT as string, 10),
+});
+
+export default database;
