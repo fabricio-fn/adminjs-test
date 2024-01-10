929f8e2f1185d94f541a1af0dc36c9f8e5a84ef7
Fabricio Nascimento <fabriciocunha040@gmail.com>
2024-01-10 17:22:09 -0300

    :card_file_box: feat: Add migration for 'menu' table

---
+++ src/db/migrations/20240110172803-menu.cjs
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('menu', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.DataTypes.TEXT,
+      },
+      thumbnailUrl: {
+        type: Sequelize.DataTypes.STRING,
+      },
+      featured: {
+        defaultValue: false,
+        type: Sequelize.DataTypes.BOOLEAN,
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('menu');
+  },
+};
