6ec1089239fb19998681fe1ec858bbc1b0b9b602
Fabricio Nascimento <fabriciocunha040@gmail.com>
2024-01-10 17:27:37 -0300

    :gear: feat: Create AdminJS resource for 'menu' table

--- src/admin/resources/index.ts
+++ src/admin/resources/index.ts
@@ -1,12 +1,18 @@
 import { ResourceWithOptions } from 'adminjs';
 
+import { Menu } from '../../db/models/Menu.js';
 import { Category } from '../../db/models/Category.js';
 
 import { categoryResourceOptions } from './category.js';
+import { menuResourceOptions } from './menu.js';
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categoryResourceOptions,
+  },
+  {
+    resource: Menu,
+    options: menuResourceOptions,
   },
 ];
