67bb6592ff7d7d8399b6225de2af746aa0b4f406
Fabricio Nascimento <fabriciocunha040@gmail.com>
2024-01-22 17:48:57 -0300

    :card_file_box: feat: Add migration for 'user' table

---
+++ src/db/migrations/20240120111404-user.cjs
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      birth: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user');
+  },
+};
